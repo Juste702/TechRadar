74b473a243ce05710db17f0b049aca43b925ba1d
Juste <justebidouzo@gmail.com>
2026-06-02 04:57:11 +0100

    feat: add scheduler to fetch articles hourly

--- back-techradar/routes/console.php
+++ back-techradar/routes/console.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use App\Services\ArticleFetcher;
+
+Schedule::call(function () {
+    (new ArticleFetcher())->fetchFromDevTo();
+})->hourly();
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
