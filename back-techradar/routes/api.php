39a3e26b5954545ea0e39d8b2fa600914416592e
Juste <justebidouzo@gmail.com>
2026-06-02 04:43:41 +0100

    feat: add ArticleController and expose GET /api/articles endpoint

--- back-techradar/routes/api.php
+++ back-techradar/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
+Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
